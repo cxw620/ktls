25b590950b81bff5e8f1933bf49d52d03dbeb030
Hantong Chen <cxwdyx620@gmail.com>
2025-08-27 10:31:47 +0800

    PR 62: partial: prepare ktls-util crate

---
+++ ktls-util/src/lib.rs
@@ -0,0 +1,20 @@
+//! Utilities for ktls crate.
+
+#![warn(
+    unsafe_code,
+    unused_must_use,
+    clippy::alloc_instead_of_core,
+    clippy::exhaustive_enums,
+    clippy::exhaustive_structs,
+    clippy::manual_let_else,
+    clippy::use_self,
+    clippy::upper_case_acronyms,
+    elided_lifetimes_in_paths,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    unused_import_braces,
+    unused_extern_crates,
+    unused_qualifications
+)]
