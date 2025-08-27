722f28e1a6279a4a9c6045e4b955990122405ba9
Hantong Chen <cxwdyx620@gmail.com>
2025-08-27 10:36:53 +0800

    PR 62: partial: implement KtlsConnector in ktls-util

--- ktls-util/src/lib.rs
+++ ktls-util/src/lib.rs
@@ -18,3 +18,60 @@
     unused_extern_crates,
     unused_qualifications
 )]
+
+use std::{io, slice};
+
+use tokio::io::{AsyncRead, AsyncReadExt};
+
+pub mod client;
+pub(crate) mod error;
+
+pub(crate) async fn read_record<S>(socket: &mut S, incoming: &mut Vec<u8>) -> io::Result<()>
+where
+    S: AsyncRead + Unpin,
+{
+    const RECORD_HDR_SIZE: usize = 5;
+
+    incoming.reserve(RECORD_HDR_SIZE);
+
+    #[allow(unsafe_code)]
+    // Safety: We just reserved enough space for the header.
+    let record_hdr = unsafe {
+        slice::from_raw_parts_mut(
+            incoming.spare_capacity_mut().as_mut_ptr().cast(),
+            RECORD_HDR_SIZE,
+        )
+    };
+
+    socket
+        .read_exact(record_hdr)
+        .await
+        .map_err(ktls::Error::IO)?;
+
+    let payload_length = u16::from_be_bytes([record_hdr[3], record_hdr[4]]) as usize;
+
+    incoming.reserve(payload_length);
+
+    #[allow(unsafe_code)]
+    // Safety: We just reserved enough space for the payload.
+    let payload = unsafe {
+        slice::from_raw_parts_mut(
+            incoming
+                .spare_capacity_mut()
+                .as_mut_ptr()
+                .add(RECORD_HDR_SIZE)
+                .cast(),
+            payload_length,
+        )
+    };
+
+    socket.read_exact(payload).await.map_err(ktls::Error::IO)?;
+
+    #[allow(unsafe_code)]
+    // Safety: We have just read data into the space we reserved.
+    unsafe {
+        incoming.set_len(incoming.len() + RECORD_HDR_SIZE + payload_length);
+    }
+
+    Ok(())
+}
