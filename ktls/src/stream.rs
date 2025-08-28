8a442032ef78c2afa84ae104e03cfc105cf03966
Hantong Chen <cxwdyx620@gmail.com>
2025-08-29 00:06:53 +0800

    refactor: replace pin-project with lite version

--- ktls/src/stream.rs
+++ ktls/src/stream.rs
@@ -11,7 +11,6 @@ pub mod impl_tokio;
 #[cfg(feature = "raw-api")]
 use std::io;
 use std::os::fd::AsFd;
-use std::pin::Pin;
 
 use rustls::client::UnbufferedClientConnection;
 use rustls::server::UnbufferedServerConnection;
@@ -23,62 +22,58 @@ use crate::stream::context::{Context, StreamState, TlsConnData};
 
 const DEFAULT_SCRATCH_CAPACITY: usize = 64;
 
-#[pin_project::pin_project(project = KTlsStreamProject, PinnedDrop)]
-/// A thin wrapper around an inner socket with kernel TLS (kTLS) offload
-/// configured.
-///
-/// This implements traits [`Read`](std::io::Read) and
-/// [`Write`](std::io::Write), [`AsyncRead`](tokio::io::AsyncRead) and
-/// [`AsyncWrite`](tokio::io::AsyncWrite) (when feature `async-io-tokio` is
-/// enabled).
-///
-/// For those who may need low-level access to the inner socket, feature
-/// `raw-api` provides an unsafe method [`as_raw`](Self::as_raw) to get a
-/// mutable reference to the inner socket.
-///
-/// ## Behaviours
-///
-/// Once a TLS `close_notify` alert from the peer is received, all subsequent
-/// read operations will return EOF.
-///
-/// Once the caller explicitly calls `(poll_)shutdown` on the stream, all
-/// subsequent write operations will return 0 bytes, indicating that the
-/// stream is closed for writing.
-///
-/// Once the stream is being dropped, a `close_notify` alert would be sent to
-/// the peer automatically before shutting down the inner socket, according to
-/// [RFC 8446, section 6.1].
-///
-/// The caller may call `(poll_)shutdown` on the stream to shutdown explicitly
-/// both sides of the stream. Currently, there's no way provided by this crate
-/// to shutdown the TLS stream write side only. For TLS 1.2, this is ideal since
-/// once one party sends a `close_notify` alert, *the other party MUST respond
-/// with a `close_notify` alert of its own and close down the connection
-/// immediately*, according to [RFC 5246, section 7.2.1]; for TLS 1.3, *both
-/// parties need not wait to receive a "`close_notify`" alert before
-/// closing their read side of the connection*, according to [RFC 8446, section
-/// 6.1].
-///
-/// [RFC 5246, section 7.2.1]: https://tools.ietf.org/html/rfc5246#section-7.2.1
-/// [RFC 8446, section 6.1]: https://tools.ietf.org/html/rfc8446#section-6.1
-pub struct KtlsStream<S>
-where
-    S: AsFd,
-{
-    #[pin]
-    inner: S,
+pin_project_lite::pin_project! {
+    #[project = KTlsStreamProject]
+    /// A thin wrapper around an inner socket with kernel TLS (kTLS) offload
+    /// configured.
+    ///
+    /// This implements traits [`Read`](std::io::Read) and
+    /// [`Write`](std::io::Write), [`AsyncRead`](tokio::io::AsyncRead) and
+    /// [`AsyncWrite`](tokio::io::AsyncWrite) (when feature `async-io-tokio` is
+    /// enabled).
+    ///
+    /// For those who may need low-level access to the inner socket, feature
+    /// `raw-api` provides an unsafe method [`as_raw`](Self::as_raw) to get a
+    /// mutable reference to the inner socket.
+    ///
+    /// ## Behaviours
+    ///
+    /// Once a TLS `close_notify` alert from the peer is received, all subsequent
+    /// read operations will return EOF.
+    ///
+    /// Once the caller explicitly calls `(poll_)shutdown` on the stream, all
+    /// subsequent write operations will return 0 bytes, indicating that the
+    /// stream is closed for writing.
+    ///
+    /// Once the stream is being dropped, a `close_notify` alert would be sent to
+    /// the peer automatically before shutting down the inner socket, according to
+    /// [RFC 8446, section 6.1].
+    ///
+    /// The caller may call `(poll_)shutdown` on the stream to shutdown explicitly
+    /// both sides of the stream. Currently, there's no way provided by this crate
+    /// to shutdown the TLS stream write side only. For TLS 1.2, this is ideal since
+    /// once one party sends a `close_notify` alert, *the other party MUST respond
+    /// with a `close_notify` alert of its own and close down the connection
+    /// immediately*, according to [RFC 5246, section 7.2.1]; for TLS 1.3, *both
+    /// parties need not wait to receive a "`close_notify`" alert before
+    /// closing their read side of the connection*, according to [RFC 8446, section
+    /// 6.1].
+    ///
+    /// [RFC 5246, section 7.2.1]: https://tools.ietf.org/html/rfc5246#section-7.2.1
+    /// [RFC 8446, section 6.1]: https://tools.ietf.org/html/rfc8446#section-6.1
+    pub struct KtlsStream<S: AsFd> {
+        #[pin]
+        inner: S,
+        ctx: Context,
+    }
 
-    /// The context of the kTLS stream.
-    ctx: Context,
-}
+    impl<S: AsFd> PinnedDrop for KtlsStream<S> {
+        fn drop(this: Pin<&mut Self>) {
+            let this = this.project();
 
-#[pin_project::pinned_drop]
-impl<S: AsFd> PinnedDrop for KtlsStream<S> {
-    fn drop(self: Pin<&mut Self>) {
-        let this = self.project();
-
-        // TODO: No need to flush? It's a no-op anyway for TcpStream / UnixStream.
-        this.ctx.shutdown(&*this.inner);
+            // TODO: No need to flush? It's a no-op anyway for TcpStream / UnixStream.
+            this.ctx.shutdown(&*this.inner);
+        }
     }
 }
 
